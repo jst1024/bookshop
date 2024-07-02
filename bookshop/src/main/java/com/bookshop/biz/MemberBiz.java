b4ee71a1bdc50f319451a7785082f83a11d03c3a
jst1024 <160584115+jst1024@users.noreply.github.com>
2024-07-02 09:25:41 +0900

    Delete bookshop directory

--- bookshop/src/main/java/com/bookshop/biz/MemberBiz.java
+++
@@ -1,9 +0,0 @@
-package com.bookshop.biz;
-
-import java.util.List;
-
-import com.bookshop.domain.Member;
-
-public interface MemberBiz {
-	public List<Member> getMemberList();
-}
--- bookshop/src/main/java/com/bookshop/per/MemberMapper.java
+++
@@ -1,9 +0,0 @@
-package com.bookshop.per;
-
-import java.util.List;
-
-import com.bookshop.domain.Member;
-
-public interface MemberMapper {
-	public List<Member> getMemberList();
-}
